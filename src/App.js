709a8d4a53f8ad55d8b4cdb53c6780e7535ccf40
Muhammad Umair <181370124@gift.edu.pk>
2022-07-27 19:35:00 +0500

    updation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
+function App() {
+  const colors = ["#8e44af", "#8e90ad", "#8e67ad", "#8e23ad"];
+  let id = colors.length;
 
+  const [bg, setBg] = useState(colors[0]);
+  const [name, setName] = useState("Click Me");
 
-
-function App = ()=> {
+  const myFunction = () => {
+    id = id === 0 ? colors.length : id - 1;
+    setBg(colors[id]);
+   
+  };
+  const valueChange = () =>{
+    setName("Hello 👩 ");
+  }
   return (
-  <div>
-    <button></button>
-  </div>
+    <div className="main" style={{ backgroundColor: bg }}>
+      <div>
+        <button onClick={() => myFunction()} onDoubleClick={() => valueChange() }>{name}</button>
+      </div>
+    </div>
   );
 }
 
