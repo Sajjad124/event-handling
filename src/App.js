47e550d280f247800abacf323f1a210cfa85c1cc
Muhammad Umair <181370124@gift.edu.pk>
2022-07-30 00:12:48 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const colors = [ "#8e90ad", "#a032a8" , "#8e23ad","#32a852"];
+  const colors = ["#8e90ad", "#a032a8", "#8e23ad", "#32a852"];
   let id = colors.length;
 
   const [bg, setBg] = useState(colors[0]);
@@ -11,15 +11,19 @@ function App() {
   const myFunction = () => {
     id = id === 0 ? colors.length : id - 1;
     setBg(colors[id]);
-   
   };
-  const valueChange = () =>{
-    setName("Hello 👩 ");
-  }
+  const valueChange = () => {
+    setName("Hello 🔆 ");
+  };
   return (
     <div className="main" style={{ backgroundColor: bg }}>
       <div>
-        <button onClick={() => myFunction()} onDoubleClick={() => valueChange() }>{name}</button>
+        <button
+          onClick={() => myFunction()}
+          onDoubleClick={() => valueChange()}
+        >
+          {name}
+        </button>
       </div>
     </div>
   );
